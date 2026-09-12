29e212b06f2d998269dc6b6e4411d7c4f6690751
dylanrkuster <160812813+dylanrkuster@users.noreply.github.com>
2026-09-12 09:08:11 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        // produce a map of { num: position }
+        const numToPos = new Map<number, number>(
+            nums.map((num, i) => [num, i])
+        );
+
+        // loop nums, looking for the pair to a given entry that produces our target
+        for (let i = 0; i < nums.length; i++) {
+            const match = target - nums[i];
+            const matchPos = numToPos.get(match);
+
+            // if we find it, return it
+            if (matchPos && matchPos !== i) {
+                return [i, matchPos];
+            }
+        }
+
+        // according to our input conditions this should never be possible
+        return [];
+    }
+}
