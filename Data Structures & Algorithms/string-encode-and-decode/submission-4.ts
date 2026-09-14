c415517178457b4561da810d43aff46056d6c6f5
dylanrkuster <160812813+dylanrkuster@users.noreply.github.com>
2026-09-14 09:21:09 -0400

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.ts
@@ -0,0 +1,53 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let strLengths = "{"
+
+        for (const str of strs) {
+            strLengths += `${str.length}|`;
+        }
+
+        // "HelloWorld{5|5|"
+        return strs.join("") + strLengths;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const strLengths = [];
+
+        // find where the encoding begins
+        let startStrLengths = str.length - 1;
+        while (str[startStrLengths] !== "{") {
+            startStrLengths--;
+        }
+
+        let curStrLength = "";
+        for (let i = startStrLengths + 1; i < str.length; i++) {
+            if (str[i] === "|") {
+                strLengths.push(Number(curStrLength));
+                curStrLength = "";
+            } else {
+                curStrLength += str[i];
+            }
+        }
+
+        const decoded = [];
+        for (const strLength of strLengths) {
+            let curStr = "";
+            for (let i = 0; i < strLength; i++) {
+                curStr += str[i];
+            }
+            decoded.push(curStr);
+            curStr = "";
+            str = str.slice(strLength);
+        }
+
+        return decoded;
+    }
+}
